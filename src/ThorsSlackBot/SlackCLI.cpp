4780463a28d9e080a32fede13bc796f435609451
Loki Astari <Loki.Astari@gmail.com>
2025-11-25 22:26:08 -0800

    Add Delete Scheduled Message

--- src/ThorsSlackBot/SlackCLI.cpp
+++ src/ThorsSlackBot/SlackCLI.cpp
@@ -2,6 +2,7 @@
 #include "ThorsSlack/SlackClient.h"
 #include "ThorsSlack/APIChatPostMessage.h"
 #include "ThorsSlack/APIChatScheduleMessage.h"
+#include "ThorsSlack/APIChatDeleteScheduledMessage.h"
 #include "ThorSerialize/JsonThor.h"
 
 using namespace std::literals::string_literals;
@@ -15,10 +16,12 @@ int main()
     using ThorsAnvil::Slack::SlackClient;
     using ThorsAnvil::Slack::API::Chat::PostMessage;
     using ThorsAnvil::Slack::API::Chat::ScheduleMessage;
+    using ThorsAnvil::Slack::API::Chat::DeleteScheduledMessage;
 
     SlackClient             client(environment.slackToken);
 
     // auto reply = client.sendMessage(PostMessage{.channel = "C09RU2URYMS", .text = "I hope the tour went well, Mr. Wonka."});
     auto reply = client.sendMessage(ScheduleMessage{.channel = "C09RU2URYMS", .post_at = time(nullptr) + 60, .text = "A timed message"});
-    std::cout << ThorsAnvil::Serialize::jsonExporter(reply);
+    client.tryMessage(DeleteScheduledMessage{.channel = "C09RU2URYMS", .scheduled_message_id = reply.scheduled_message_id,});
+    //std::cout << ThorsAnvil::Serialize::jsonExporter(reply);
 }
