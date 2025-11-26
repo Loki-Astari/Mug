99c8ccd30b02be3d907618040cb4d5b0b3cfa1f1
Loki Astari <Loki.Astari@gmail.com>
2025-11-25 21:50:51 -0800

    Add missing files

---
+++ src/ThorsSlack/API.h
@@ -0,0 +1,32 @@
+#ifndef THORSANVIL_SLACK_API_H
+#define THORSANVIL_SLACK_API_H
+
+#include "SlackBlockKit.h"
+#include "ThorsSlackConfig.h"
+#include "ThorSerialize/Traits.h"
+
+#include <string>
+
+namespace ThorsAnvil::Slack::API
+{
+
+using OptBool       = std::optional<bool>;
+using OptString     = std::optional<std::string>;
+using VecString     = std::vector<std::string>;
+using OptVecString  = std::optional<std::vector<std::string>>;
+
+struct Reply
+{
+    bool                        ok;
+    OptString                   error;
+    OptVecString                errors;
+    OptString                   warning;
+    OptVecString                warnings;
+};
+
+}
+
+ThorsAnvil_MakeTrait(ThorsAnvil::Slack::API::Reply, ok, error, errors, warning, warnings);
+
+
+#endif
